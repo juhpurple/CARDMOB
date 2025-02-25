1b3237b8c914bf04ab0cfed9b48a8f6878bd52f0
juhpurple <jussara.m@aluno.ifsp.edu.br>
2025-02-25 10:20:07 -0300

    revisao react 1

---
+++ ETAPA1/react-exemplos/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  // function updateCount() {
+  //   setCount(count+1);
+  // }
+
+  // arraw function
+
+  const updateCount = (count) => {
+    return count + 1
+  }
+
+  const updateCount1 = () => count + 1;
+
+  const dados = {
+    "nome" : "fulano",
+    "atualiza": (novo_nome) => `Novo nome e ${novo_nome}`,
+    "endereco": {
+      "rua": "geu",
+      "numero": "111",
+      "complementos": ["casa", "na esquina"]
+    }
+
+  };
+
+  dados.atualiza("gerson");
+  dados.endereco.complementos[1];
+
+  return (
+    <>
+    <Counter title="Contador"/>
+    <Counter initial="100"/>
+    </>
+  )
+}
+
+export default App
---
+++ ETAPA1/react-exemplos/src/assets/components/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function Counter({title, initial = 0}) {
+  const [count, setCount] = useState(Number(initial))
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default Counter
