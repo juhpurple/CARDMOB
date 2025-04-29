fcbf5ceab1878ff01b1b641979f84aee79cf85d8
JUSSARA MARTINS DE SOUSA <CG302752X@car.ifsp>
2025-04-29 09:48:24 -0300

    CRUD simples: addItem

--- ETAPA-2/app-react-exemplos/App.js
+++ ETAPA-2/app-react-exemplos/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput } from 'react-native';
 
 export default function App() {
   const [counter, setCounter] = useState(0)
+  // CRUD
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState('');
+  const [editItemId, setEditItemId] = useState(null);
+  const [editItemText, setEditItemText] = useState('');
 
   const incrementCounter = () => {
     setCounter(counter + 1);
@@ -12,11 +17,24 @@ export default function App() {
   const decrementCounter = () => {
     setCounter(counter - 1);
   }
+
+  const addItem = () => {
+    if (text.trim() === '') {return};
+    const newItem = {
+      id: Math.random().toString(),
+      text: text.trim(),
+    }
+    setItems([...items, newItem]);
+    setText('');
+    console.log(items);
+  }
+
   return (
     <View style={styles.container}>
+      <TextInput style={styles.input} value={text} onChangeText={setText} placeholder='Enter text item'/>
+      <Button title='Add Item' onPress={addItem} />
       <Text style={styles.text}>Olá App React Native - Atualiza!</Text>
       <Image source={{url: "https://plcsum.photos/200"}} style={{width: 200, height: 200}}/>
-
 
       <StatusBar style="auto" />
       <Text style={styles.text}>Counter: {counter}</Text>
