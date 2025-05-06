54b152fb287710fdc41921f3297c65261969bce3
juhpurple <jussara.m@aluno.ifsp.edu.br>
2025-05-06 10:21:14 -0300

    Add List component (WIP)

--- ETAPA-2/app-react-exemplos/App.js
+++ ETAPA-2/app-react-exemplos/App.js
@@ -1,122 +1,14 @@
-import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, Image, TextInput, FlatList } from "react-native";
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Image, TextInput, FlatList } from 'react-native';
+
+import List from './components/List';
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-  /// CRUD em memória
-  const [items, setItems] = useState([]);
-  const [text, setText] = useState("");
-  const [editItemId, setEditItemId] = useState(null);
-  const [editItemText, setEditItemText] = useState("");
-
-  const incrementCounter = () => {
-    setCounter(counter + 1);
-  };
-
-  const decrementCounter = () => {
-    setCounter(counter - 1);
-  };
-
-  // Create
-  const addItem = () => {
-    if (text.trim() === "") {
-      return;
-    }
-    const newItem = {
-      id: Math.random().toString(),
-      text: text.trim(),
-    };
-    setItems([...items, newItem]);
-    setText("");
-    console.log(items);
-  };
-
-  // Update
-  const updateItem = (id) => {
-    setItems(
-      items.map((item) => {
-        if (item.id === id) {
-          return { ...item, text: editItemText };
-        }
-        return item;
-      })
-    );
-    setEditItemId(null);
-    setEditItemText("");
-  };
-
-  // Delete
-  const deleteItem = (id) => {
-    setItems(items.filter((item) => item.id !== id));
-  };
-
-  // Read
-  const renderItem = ({ item }) => {
-    if (item.id != editItemId) {
-      return (
-        <View style={styles.item}>
-          <Text style={styles.itemText}>{item.text}</Text>
-          <View style={styles.buttons}>
-            <Button
-              title="Edit"
-              onPress={() => {
-                setEditItemId(item.id);
-              }}
-            ></Button>
-            <Button
-              title="Delete"
-              onPress={() => {
-                deleteItem(item.id);
-              }}
-            ></Button>
-          </View>
-        </View>
-      );
-    } else {
-      // Um item esta sendo editado
-      return (
-        <View style={styles.item}>
-          <TextInput
-            style={styles.editInput}
-            onChangeText={setEditItemText}
-            value={editItemText}
-            autoFocus
-          />
-          <Button title="Update" onPress={() => updateItem(item.id)}></Button>
-        </View>
-      );
-    }
-  };
-
+ 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        value={text}
-        onChangeText={setText}
-        placeholder="Enter text item"
-      />
-      <Button title="Add Item" onPress={addItem} />
-      <FlatList
-        data={items}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-        style={styles.list}
-      />
-      <Text style={styles.text}>Olá App React Native - Atualiza!</Text>
-      <Image
-        source={{ uri: "https://picsum.photos/200" }}
-        style={{ width: 200, height: 200 }}
-      />
-
-      <StatusBar style="auto" />
-      {/* <Text style={styles.text}>Counter: {counter}</Text>
-
-      <View style={styles.buttonContainer}>
-        <Button title='Increment' onPress={incrementCounter} />
-        <Button title='Decrement' onPress={decrementCounter} />
-      </View> */}
+      <List />
     </View>
   );
 }
@@ -131,11 +23,11 @@ const styles = StyleSheet.create({
     fontSize: 24,
   },
   buttonContainer: {
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   input: {
     height: 40,
-    borderColor: "gray",
+    borderColor: 'gray',
     borderWidth: 1,
     marginBottom: 10,
     paddingHorizontal: 10,
@@ -144,12 +36,12 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   item: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     marginBottom: 10,
     padding: 10,
-    backgroundColor: "#f0f0f0",
+    backgroundColor: '#f0f0f0',
     borderRadius: 5,
   },
   itemText: {
@@ -157,13 +49,13 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   buttons: {
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   editInput: {
     flex: 1,
     marginRight: 10,
-    borderColor: "gray",
+    borderColor: 'gray',
     borderWidth: 1,
     paddingHorizontal: 10,
-  },
+  }
 });
