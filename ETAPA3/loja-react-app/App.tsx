9b1106fd6e472cd396c9bf4ae6fbf32346505345
juhpurple <jussara.m@aluno.ifsp.edu.br>
2025-08-12 08:43:24 -0300

    Inclui ThemeContext

--- ETAPA3/loja-react-app/App.tsx
+++ ETAPA3/loja-react-app/App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { ThemeProvider} from './src/contexts/ThemeContext';
 import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
   return (
-    <HomeScreen />
+    <ThemeProvider>
+      <HomeScreen />
+    </ThemeProvider>
+
   );
 }
 
