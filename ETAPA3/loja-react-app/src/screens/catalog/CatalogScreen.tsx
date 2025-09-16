6e10b8cc3f832bd9bd9ee8ca9ae4321435a61637
juhpurple <jussara.m@aluno.ifsp.edu.br>
2025-09-16 08:13:24 -0300

    Tela de catálogo com backend

--- ETAPA3/loja-react-app/src/screens/catalog/CatalogScreen.tsx
+++ ETAPA3/loja-react-app/src/screens/catalog/CatalogScreen.tsx
@@ -1,35 +1,60 @@
-import  React, { useContext } from "react";
-import { View, Text, FlatList, StyleSheet} from "react-native";
+import React, { useContext, useState, useEffect } from "react"; // alterado
+import { View, Text, FlatList, StyleSheet} from 'react-native';
 
 import CatalogCard from "./CatalogCard";
 
-// Todos: importar o serviço da recuperação de catalog
+// Todo: importar o serviço de recuperação do catalog
+import { getCatalog } from '../../services/catalogServices'; // novo
 
-const CatalogScreen = ({navigation}: any) => {
-  const handleBuyPress = (product: any) => {
-    // 1 - Adicionar ao carrinho
-    // 2 - Ir para a tela do carrinho
-    console.log(product);
-  };
+const CatalogScreen = ({navigation} : any) => {
+    const [catalog, setCatalog] = useState<any[]>([]); // novo
 
-  const renderItem = ({product}: any) => (
-    <CatalogCard product={product} onBuyPress={() => handleBuyPress(product)} />
-  );
+    // bloco novo
+    useEffect(() => {
+        const fetchCatalog = async () => {
+            try {
+                const data = await getCatalog();
+                setCatalog(data);
+            }
+            catch (error) {
+                console.error('Erro ao buscar o catálogo:', error);
+            }
+        };
+        fetchCatalog();
+        console.log(catalog);
+    }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>Menu</Text>
-      <FlatList data={[]} renderItem={renderItem} keyExtractor={(item: any) => item.id}/>
-    </View>
-  );
+    const handleBuyPress = (product : any) => {
+        // 1 - Adicionar ao carrinho
+        // 2 - Ir para a tela do carrinho
+        console.log(product);
+    };
+
+    const renderItem = ({ item }: any) => ( // alterado
+        <CatalogCard 
+            product={item} // alterado
+            onBuyPress={() => handleBuyPress(item)} // alterado
+        />
+    );
+
+    return (
+        <View style={styles.container}>
+            <Text>Menu</Text>
+            <FlatList 
+                data={catalog} // alterado
+                renderItem={renderItem}
+                keyExtractor={(item: any) => item.id.toString()} // alterado
+            />
+        </View>
+    );
 };
 
 export default CatalogScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 15,
-    backgroundColor: '#F8F8F8',
-  }
-})
+    container: {
+        flex: 1,
+        padding: 15,
+        backgroundColor: '#F8F8F8',
+    }
+});
